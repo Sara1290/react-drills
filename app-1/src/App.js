703cbc11987acd95502f118aeea054231f5da95f
Sara Reed <reedsara90@gmail.com>
2021-03-28 18:34:04 -0600

    apps 1 through 6 done

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -7,20 +7,7 @@ function App() {
   return (
     <div className="App">
       <TextBox/>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />                      // i know this is just the logo and learn react link but i'm too scared to delete it
-        <p>
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      
     </div>
   );
 }
